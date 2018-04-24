cd464d1468cc55821426f8eaa87686d2f09c284d
Ahmadi.Mohammad <ahmadi80@gmail.com>
2018-04-24 14:49:12 +0430

    Integrating passport into project

--- server.js
+++ server.js
@@ -3,15 +3,33 @@ const express = require('express');
 const path = require('path');
 const http = require('http');
 const bodyParser = require('body-parser');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var mongoose = require('mongoose');
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
 
 // Get our API routes
 const api = require('./server/routes/api');
-
 const app = express();
+
+// Get Models references
+var Account = require('./server/models/account');
 
 // Parsers for POST data
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(logger('dev'));
+app.use(cookieParser());
+app.use(require('express-session')({
+    secret: 'keyboard cat',
+    resave: false,
+    saveUninitialized: false
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 // Point static path to dist
 app.use(express.static(path.join(__dirname, 'dist')));
@@ -23,6 +41,47 @@ app.use('/api', api);
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, 'dist/index.html'));
 });
+
+// passport config
+passport.use(new LocalStrategy(Account.authenticate()));
+passport.serializeUser(Account.serializeUser());
+passport.deserializeUser(Account.deserializeUser());
+
+// mongoose
+mongoose.connect('mongodb://localhost/PersianDevsDb');
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+});
+
+// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+    app.use(function(err, req, res, next) {
+        res.status(err.status || 500);
+        res.render('error', {
+            message: err.message,
+            error: err
+        });
+    });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+        message: err.message,
+        error: {}
+    });
+});
+
+
 
 /**
  * Get port from environment and store in Express.
--- server/routes/api.js
+++ server/routes/api.js
@@ -1,5 +1,7 @@
-const express = require('express');
-const router = express.Router();
+var express = require('express');
+var passport = require('passport');
+var Account = require('../models/account');
+var router = express.Router();
 
 // declare axios for making http requests
 const axios = require('axios');
@@ -21,6 +23,22 @@ router.get('/posts', (req, res) => {
     .catch(error => {
       res.status(500).send(error)
     });
+});
+
+router.post('/register', function(req, res) {
+  Account.register(new Account({ username : req.body.username }), req.body.password, function(err, account) {
+      if (err) {
+          return res.render('register', { account : account });
+      }
+
+      passport.authenticate('local')(req, res, function () {
+          res.redirect('/');
+      });
+  });
+});
+
+router.post('/login', passport.authenticate('local'), function(req, res) {
+  res.redirect('/');
 });
 
 module.exports = router;
