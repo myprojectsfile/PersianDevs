f2c552f5778de67b294d7f90368cd99375240bf2
myprojectsfile <myprojectsfile@hotmail.com>
2018-05-04 20:12:30 +0430

    after migrated to "bull" queue library

--- server/routes/dockerApi.js
+++ server/routes/dockerApi.js
@@ -2,16 +2,22 @@ var express = require('express');
 var router = express.Router();
 var docker = require('./docker-lib');
 var Promise = require("bluebird");
-const PQueue = require('p-queue');
 const uuidv1 = require('uuid/v1');
+var Queue = require('bull');
 
-// var Queue = require('promise-queue');
 global.Promise = Promise;
-// Queue.configure(require('bluebird'));
 var maxConcurrent = 1;
 var maxQueue = Infinity;
-// var queue = new Queue(maxConcurrent, maxQueue);
-const queue = new PQueue({ concurrency: maxConcurrent });
+
+var dockerDownQueue = new Queue('docker_download_queue');
+
+
+dockerDownQueue.process(function (job) {
+    return docker.downloadImage(job.data.image, job.data.tag, job.opts.jobId);
+
+    return Promise.reject(new Error(`error in dwonloaing docker image with jobId:${job.opts.jobId}`));
+    return Promise.resolve({ jobId: job.opts.id });
+});
 
 /* GET api listing. */
 router.get('/', (req, res) => {
@@ -21,22 +27,29 @@ router.get('/', (req, res) => {
 
 router.post('/downloadImage', function (req, res) {
     // generate taksId;
-    id = uuidv1();
-
-    var image = req.body.image;
-    var tag = req.body.tag;
+    var _jobId = uuidv1();
+    var _image = req.body.image;
+    var _tag = req.body.tag;
 
     // add image to download queue
-    queue.add(() => docker.downloadImage(image, tag, id)
+    dockerDownQueue.add({ image: _image, tag: _tag }, { jobId: _jobId })
         .then(
-            (taskId) => { console.log(`download task with id ${taskId} completed Successfully.`) })
+            (job) => {
+                console.log(`+++++ Added +++++ download job with id ${job.opts.jobId} added to queue Successfully.`);
+                res.status(200).send();
+            })
         .catch(
             (error) => {
                 console.log('error message:' + error);
-            }));
-
-    res.status(200).send();
+            });
 });
 
+dockerDownQueue.on('active', function (job, jobPromise) {
+    console.log(`<<<<< Started >>>>> the download job with id ${job.opts.jobId} is started`);
+})
+
+dockerDownQueue.on('completed', function (job, jobPromise) {
+    console.log(`>>>>> Completed <<<<< the download job with id ${job.opts.jobId} is completed`);
+})
 
 module.exports = router;
